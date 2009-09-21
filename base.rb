705cb0bd9d9d5bbf43a395d4b206c1d7057811f1
Ritchie Young <ritchie@gmail.com>
2009-09-21 14:00:22 +0800

    removed rspec question

--- base.rb
+++ base.rb
@@ -1,8 +1,3 @@
-if yes?("Do you want to use RSpec for testing?")
-  plugin "rspec", :git => "git://github.com/dchelimsky/rspec.git"
-  plugin "rspec-rails", :git => "git://github.com/dchelimsky/rspec-rails.git"
-  generate :rspec
-end
 
 generate :nifty_layout
 
@@ -10,7 +5,6 @@ git :init
 
 run "echo 'TODO add readme content' > README"
 run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
-run "cp config/database.yml config/example_database.yml"
 
 file ".gitignore", <<-END
 log/*.log
